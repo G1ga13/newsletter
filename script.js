2095f360c807cb09a3bf93633691b34112529d63
Giga <gigashekelashvili1@gmail.com>
2025-11-16 15:19:54 +0400

    newsletter

---
+++ script.js
@@ -0,0 +1,31 @@
+let number = 101;
+if (number * number > 100) {
+  console.log("მეტია ასზე");
+} else {
+  console.log("ნაკლებია ასზე");
+}
+
+let number1 = 49;
+if (number1 > 50 && number1 < 100) {
+  console.log("რიცხვი არის 50-ზე მეტი და 100-ზე ნაკლები");
+} else {
+  console.log("რიცხვი არ ასრულებს მოცემულ პირობას");
+}
+
+let negativeNum = 1;
+if (negativeNum <= 0) {
+  console.log("მოცემული რიცხვი ნაკლებია ან ტოლია 0-ის");
+} else {
+  console.log("მოცემული რიცხვი არ არის ნაკლები ან ტოლი 0-ის");
+}
+
+let width = 10;
+let width1 = 10;
+let length = 20;
+let length2 = 20;
+
+if (width + width1 + length + length2 > 50) {
+  console.log("პერიმეტრი მეტია 50-ზე");
+} else {
+  console.log("პერიმეტრი არ არის მეტი 50-ზე");
+}
